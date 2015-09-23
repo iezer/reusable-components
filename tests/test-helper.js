d5a51e4168736e9723f3c220a08b4a0498fa2033
Isaac Ezer <isaac.ezer@gmail.com>
2015-09-22 22:03:03 -0400

    fake server

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,16 @@
 import resolver from './helpers/resolver';
+import FakeServer from 'ember-cli-fake-server';
+
 import {
   setResolver
 } from 'ember-qunit';
 
 setResolver(resolver);
+
+QUnit.testStart(function(){
+  FakeServer.start();
+});
+
+QUnit.testDone(function(){
+  FakeServer.stop();
+});
