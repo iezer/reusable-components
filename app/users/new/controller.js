530b0a2dc6eccbb6df8df337be0fb7f254f58ff3
Isaac Ezer <isaac.ezer@gmail.com>
2015-09-24 12:30:05 -0400

    errors

--- app/users/new/controller.js
+++ app/users/new/controller.js
@@ -3,14 +3,22 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   firstName: null,
   lastName: null,
+  errorMessage: null,
 
   actions: {
     saveUser() {
+      this.set("errorMessage", null);
+      let user =
       this.store.createRecord('user', {
         firstName: this.get('firstName'),
         lastName: this.get('lastName')
-      }).save().then(() => {
+      });
+
+      user.save().then(() => {
         this.transitionToRoute("users.index");
+      }).catch((e) => {
+        let errorDetails = e.errors.mapBy('detail').join(',');
+        this.set('errorMessage', errorDetails);
       });
     }
   }
