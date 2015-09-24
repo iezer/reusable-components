c8a8f4c52aa3c5dc9de43076d2001de3da3821a5
Isaac Ezer <isaac.ezer@gmail.com>
2015-09-24 18:54:25 -0400

    action on component

--- app/components/user-form.js
+++ app/components/user-form.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveUser: function() {
+      let payload = {
+        firstName: this.get('firstName'),
+        lastName: this.get('lastName')
+      };
+
+      this.sendAction('action', payload, this.get('model'));
+    }
+  }
 });
